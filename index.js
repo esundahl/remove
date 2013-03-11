3f6554484a5cbc39aaa715a457580615e6280d6a
Erik Sundahl <esundahl@gmail.com>
2013-03-11 08:41:18 -0700

    fixes tests

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 
 module.exports = function (args) {
-
   if (arguments.length === 1) {
     args.remove = remove;
     return;
   }
-
   var arr = Array.prototype.shift.call(arguments);
   return remove.apply(arr, arguments);
 }
@@ -13,6 +11,7 @@ module.exports = function (args) {
 function remove (from, to) {
   var rest = this.slice((to || from) + 1 || this.length);
   this.length = from < 0 ? this.length + from : from;
-  return this.push.apply(this, rest);
+  this.push.apply(this, rest);
+  return this;
 }
 
--- test/remove.js
+++ test/remove.js
@@ -5,6 +5,7 @@
  */
 
 var Remove = require('remove')
+var isArray = require('yields-isArray')
 
 
 /**
@@ -17,35 +18,92 @@ describe('Remove', function(){
 
   beforeEach(function(){
     array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    Remove(array)
   })
 
   it('should act as a normal function', function(){
-    Remove(array, 1, 2, -10, -9)
-    array.should.include(1, 4, 5, 6, 7, 8)
-    array.should.not.include(2, 3, 10, 9)
+
+    var result
+
+    // Function
+    Remove(array, 1, 3)
+    array.should.eql([1,5,6,7,8,9,10])
+
+    // Mixin
+    Remove(array)
+    array.remove(0)
+    result = [5,6,7,8,9,10]
+    Remove(result)
+    array.should.eql(result)
+
   })
 
   it('should act as a mixin', function(){
+    Remove(array)
     array.remove.should.be.a('function')
+    isArray(array).should.equal(true)
   })
 
   it('should remove item from beginning', function(){
-    array.remove(1)
-    array.should.include(1, 3, 4, 5, 6, 7, 8, 9, 10)
-    array.should.not.include(2)
+
+    var result
+
+    // Function
+    Remove(array, 0)
+    array.should.eql([2,3,4,5,6,7,8,9,10])
+
+    // Mixin
+    Remove(array)
+    array.remove(0)
+    result = [3,4,5,6,7,8,9,10]
+    Remove(result)
+    array.should.eql(result)
+
   })
 
   it('should remove item from the end', function(){
+
+    var result
+
+    // Function
+    Remove(array, -1)
+    array.should.eql([1,2,3,4,5,6,7,8,9])
+
+    // Mixin
+    Remove(array)
     array.remove(-1)
-    array.should.include(1, 3, 4, 5, 6, 7, 8, 9)
-    array.should.not.include(10)
+    result = [1,2,3,4,5,6,7,8]
+    Remove(result)
+    array.should.eql(result)
+
   })
 
-  it('should remove multiple items', function(){
-    array.remove(2, 4, -1 -2)
-    array.should.include(1, 2, 4, 6, 7, 8)
-    array.should.not.include(3, 5, 9, 10)
+  it('should accept a range', function(){
+
+    var result
+
+    // Function
+    Remove(array, 1, -4)
+    array.should.eql([1,8,9,10])
+
+    // Mixin
+    Remove(array)
+    array.remove(1,-2)
+    result = [1,10]
+    Remove(result)
+    array.should.eql(result)
+
+  })
+
+  it('should be chainable', function(){
+
+    var result
+
+    Remove(array)
+    array.remove(1).remove(5).remove(-1)
+    result = [1,3,4,5,6,8,9]
+    Remove(result)
+    array.should.eql(result)
+
   })
 
   it('should extend Array.prototype', function(){
